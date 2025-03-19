c566ff195edbe79fd33fd9c2b9bc1616e74b3d82
ClaraDraper-NOAA <Clara.Draper@noaa.gov>
2025-03-19 17:24:37 +0000

    Re-arranged python for inserting single file, ready for ensembles. Also removed need to specify the static file. Results are zero-diff.

--- replace_soil_snow.py
+++ replace_soil_snow.py
@@ -3,7 +3,10 @@ import sys
 import numpy as np
 from netCDF4 import Dataset
 
-if (len(sys.argv) != 6): 
+
+##############################
+# get args
+if (len(sys.argv) != 5): 
         for i, arg in enumerate(sys.argv):
                 print(f"Argument {i}: {arg}")
         str = f" ** Error: expecting 5 arguments: \n ** YYYYMMDDHH resolution static_file directory_tiles directory_vector"
@@ -11,19 +14,14 @@ if (len(sys.argv) != 6):
 
 datestring   = sys.argv[1]
 res          = sys.argv[2]
-static_file  = sys.argv[3]
-tile_dir     = sys.argv[4] 
-vector_dir   = sys.argv[5]
+tile_dir     = sys.argv[3] 
+vector_dir   = sys.argv[4]
 
 print_high_snow_removal = True
 print_low_snow_removal = False
 
-# get veg type
-ncid = Dataset(static_file,'r')
-vegetation_type = ncid['vegetation_category'][:]
-ncid.close()
-
-# read in spun-up variables
+##############################
+# read in spun-up variables on vector
 vec_date = f"{datestring[:4]}-{datestring[4:6]}-{datestring[6:8]}_{datestring[8:10]}"
 vector_file = vector_dir+"ufs_land_restart."+vec_date+"-00-00.nc"
 
@@ -37,42 +35,15 @@ vec_swe = ncid.variables['snow_water_equiv'][0, :]
 
 ncid.close()
 
+num_in_vector = len(vec_stc[0, :])
+print(f"Num in vector: {num_in_vector}")
+
+##############################
+# Loop over tile files
+nloc = -1
 high_snow_removal = 0
 low_snow_removal = 0
 
-# Adjust snow parameters based on thresholds
-for iloc in range(len(vec_snd)):
-    if vec_snd[iloc] > 2000.0 and vegetation_type[iloc] == 15:
-        reduction_factor = 2000.0 / vec_snd[iloc]
-        if print_high_snow_removal:
-            print(f"Reducing glacier location with depth = {vec_snd[iloc]} by factor = {reduction_factor}")
-        vec_snd[iloc] = 2000.0
-        vec_swe[iloc] *= reduction_factor
-        high_snow_removal += 1
-
-    if vec_snd[iloc] > 10000.0:
-        reduction_factor = 10000.0 / vec_snd[iloc]
-        if print_high_snow_removal:
-            print(f"Reducing non-glacier location with depth = {vec_snd[iloc]} by factor = {reduction_factor}")
-        vec_snd[iloc] = 10000.0
-        vec_swe[iloc] *= reduction_factor
-        high_snow_removal += 1
-
-    if (0.0 < vec_snd[iloc] < 1.0) or (0.0 < vec_swe[iloc] < 0.01):
-        if print_low_snow_removal:
-            print(f"Removing location with SWE = {vec_swe[iloc]} and depth = {vec_snd[iloc]}")
-        vec_snd[iloc] = 0.0
-        vec_swe[iloc] = 0.0
-        low_snow_removal += 1
-
-num_in_vector = len(vec_stc[0, :])
-print(f"Num in vector: {num_in_vector}")
-print(f"Num high_snow_removal: {high_snow_removal}")
-print(f"Num low_snow_removal: {low_snow_removal}")
-
-nloc = -1
-
-# Loop over tiles
 for itile in range(1, 7):
     print(f"Starting tile: {itile}")
 
@@ -80,42 +51,68 @@ for itile in range(1, 7):
 
     ncid = Dataset(tile_file, 'r+')
 
-    inmask = ncid.variables['vtype'][0, :, :]
-    sfc_smc = ncid.variables['smc'][:]
-    sfc_slc = ncid.variables['slc'][:]
-    sfc_stc = ncid.variables['stc'][:]
-    sfc_swe = ncid.variables['weasdl'][:]
-    sfc_snd = ncid.variables['snodl'][:]
+    tile_veg = ncid.variables['vtype'][0, :, :]
+    tile_smc = ncid.variables['smc'][:]
+    tile_slc = ncid.variables['slc'][:]
+    tile_stc = ncid.variables['stc'][:]
+    tile_swe = ncid.variables['weasdl'][:]
+    tile_snd = ncid.variables['snodl'][:]
 
-    ndims = inmask.shape
-    print(f"Num vtype /= 0: {np.sum(inmask > 0)}")
+    ndims = tile_veg.shape
 
     for idim0 in range(ndims[0]):
         for idim1 in range(ndims[1]):
-            if inmask[idim0, idim1] != 0:
+            if tile_veg[idim0, idim1] != 0:
                 nloc += 1
 
-                sfc_smc[0, :, idim0, idim1] = vec_smc[:, nloc]
-                sfc_slc[0, :, idim0, idim1] = vec_slc[:, nloc]
-                sfc_stc[0, :, idim0, idim1] = vec_stc[:, nloc]
-                sfc_swe[0, idim0, idim1] = vec_swe[nloc]
-                sfc_snd[0, idim0, idim1] = vec_snd[nloc]
+                # insert vector values
+                tile_smc[0, :, idim0, idim1] = vec_smc[:, nloc]
+                tile_slc[0, :, idim0, idim1] = vec_slc[:, nloc]
+                tile_stc[0, :, idim0, idim1] = vec_stc[:, nloc]
+                tile_swe[0, idim0, idim1] = vec_swe[nloc]
+                tile_snd[0, idim0, idim1] = vec_snd[nloc]
+
+                # boundary checks
+                if tile_snd[0,idim0,idim1] > 2000.0 and tile_veg[idim0,idim1] == 15: # glaciers
+                    reduction_factor = 2000.0 / tile_snd[0,idim0,idim1]
+                    if print_high_snow_removal:
+                        print(f"Reducing glacier location with depth = {tile_snd[0,idim0,idim1]} by factor = {reduction_factor}")
+                    tile_snd[0,idim0,idim1] = 2000.0
+                    tile_swe[0,idim0,idim1] *= reduction_factor
+                    high_snow_removal += 1
+
+                if tile_snd[0,idim0,idim1] > 10000.0:
+                    reduction_factor = 10000.0 / tile_snd[0,idim0,idim1]
+                    if print_high_snow_removal:
+                        print(f"Reducing non-glacier location with depth = {tile_snd[0,idim0,idim1]} by factor = {reduction_factor}")
+                    tile_snd[0,idim0,idim1] = 10000.0
+                    tile_swe[0,idim0,idim1] *= reduction_factor
+                    high_snow_removal += 1
+
+                if (0.0 < tile_snd[0,idim0,idim1] < 1.0) or (0.0 < tile_swe[0,idim0,idim1] < 0.01):
+                    if print_low_snow_removal:
+                        print(f"Removing location with SWE = {tile_swe[0,idim0,idim1]} and depth = {tile_snd[0,idim0,idim1]}")
+                    tile_snd[0,idim0,idim1] = 0.0
+                    tile_swe[0,idim0,idim1] = 0.0
+                    low_snow_removal += 1
 
     num_in_tiles = nloc + 1
     print(f"Number of cumulative locs: {num_in_tiles}")
 
     # Update the NetCDF variables
-    ncid.variables['smc'][:] = sfc_smc
-    ncid.variables['slc'][:] = sfc_slc
-    ncid.variables['stc'][:] = sfc_stc
-    ncid.variables['weasdl'][:] = sfc_swe
-    ncid.variables['snodl'][:] = sfc_snd
+    ncid.variables['smc'][:] = tile_smc
+    ncid.variables['slc'][:] = tile_slc
+    ncid.variables['stc'][:] = tile_stc
+    ncid.variables['weasdl'][:] = tile_swe
+    ncid.variables['snodl'][:] = tile_snd
 
     ncid.close()
 
 if num_in_tiles != num_in_vector:
-    print("Number in tiles != number in vector")
-    exit()
+    str = f" ** Error: Number in tiles != number in vector"
+    sys.exit(str)
 
+print(f"Num high_snow_removal: {high_snow_removal}")
+print(f"Num low_snow_removal: {low_snow_removal}")
 print("Processing completed.")
 
